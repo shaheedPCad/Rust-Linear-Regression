b055aa6197d9ca1b34473f5a6de5abd45d2a4ead
Shaheed <shaheedPCad@gmail.com>
2022-08-29 17:57:55 -0400

    Added function to calculate mean.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,26 @@ struct LinearRegression{
     label: Vec<u32>,
 }
 
+impl LinearRegression{
+    fn get_mean(&self) -> u32{
+        let sum: u32 = self.data.iter().sum();
+        let length: usize = self.data.len();
+        sum / length;
+    }
+}
+
 fn main(){
-    println!("Linear regression");
+    let data: Vec<u32> = vec![1, 2, 3, 4, 5];
+    let label: Vec<u32> = vec![3, 4, 5, 6, 7];
+    let regress = LinearRegression{
+        data,
+        label
+    };
+
+    let sum: u32 = regress.get_mean();
+
+
+    println!("Linear regression {}", sum);
+
+
 }
